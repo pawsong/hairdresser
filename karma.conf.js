b129a19569bb3eb6571a36d74f0b6f4bc5496983
Giff Song <giff.song@gmail.com>
2015-09-28 20:08:06 +0900

    Use zuul for executing sauce labs testing

--- karma.conf.js
+++ karma.conf.js
@@ -3,22 +3,7 @@ var browsers;
 var reporters;
 var webpackConfig;
 
-if (process.env.TEST_SAUCELABS === 'true') {
-  // Sauce Labs report
-  browsers = [
-    'slIE8',
-    'slIE9',
-    'slIE10',
-    'slIE11',
-    'slChrome',
-    'slFireFox',
-    'slIphone',
-    'slAndroid',
-    'slSafari',
-  ];
-  reporters = ['mocha', 'saucelabs'];
-  webpackConfig = require('./webpack/test.config');
-} else if (process.env.TEST_COVERALLS === 'true') {
+if (process.env.TEST_COVERALLS === 'true') {
   // Coveralls report
   browsers = ['ChromeTravisCI'];
   reporters = ['mocha', 'coverage', 'coveralls'];
@@ -85,60 +70,6 @@ module.exports = function(config) { // eslint-disable-line func-names
       IE11: {
         base: 'IE',
         'x-ua-compatible': 'IE=EmulateIE11',
-      },
-      slIE8: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '8',
-      },
-      slIE9: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 7',
-        version: '9',
-      },
-      slIE10: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8',
-        version: '10',
-      },
-      slIE11: {
-        base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11',
-      },
-      slChrome: {
-        base: 'SauceLabs',
-        browserName: 'chrome',
-        platform: 'Windows 7',
-        version: '37',
-      },
-      slFireFox: {
-        base: 'SauceLabs',
-        browserName: 'firefox',
-        platform: 'Windows 7',
-        version: '32',
-      },
-      slIphone: {
-        base: 'SauceLabs',
-        browserName: 'iphone',
-        platform: 'OS X 10.9',
-        version: '7.1',
-      },
-      slAndroid: {
-        base: 'SauceLabs',
-        browserName: 'android',
-        platform: 'Linux',
-        version: '4.4',
-      },
-      slSafari: {
-        base: 'SauceLabs',
-        browserName: 'safari',
-        platform: 'OS X 10.9',
-        version: '7',
       },
     },
 
