387c687132e512405bbdeeac326997db1f3c6b7d
Giff Song <giff.song@gmail.com>
2016-11-07 02:39:06 +0900

    Export Override and AttrsObject

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import Hairdresser from './Hairdresser';
 export { Hairdresser }
+
+import Override from './Override';
+export { Override }
+
+import { AttrsObject } from './Attrs';
+export { AttrsObject }
